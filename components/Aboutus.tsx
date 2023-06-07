0698dc2a12269252f18d0021cce6e207c8132bd3
jehianth <jehianbusiness@gmail.com>
2023-06-08 05:56:18 +0700

    fix: about us and av

--- components/Aboutus.tsx
+++ components/Aboutus.tsx
@@ -1,41 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import Image from 'next/image'
 
 const Aboutus = () => {
-    const benefit = [
-        {
-            name: 'Products',
-            desc: 'We have diversity of product, Looking for Figma? or Action Figure? We got it for you and you only!',
-        },
-        {
-            name: 'Quality Product',
-            desc: 'Our product is original and at the best condition! just like you!',
-        },
-        {
-            name: 'Solid Packaging',
-            desc: 'Worry not, your product will be in good condition when arrived.',
-        },
-        {
-            name: 'Payment',
-            desc: 'We make it easy for you to make payments, After all who likes hassle?',
-        },
-    ];
     return (
         <section id='aboutus' className='bg-white w-full flex flex-col items-center justify-center'>
             <h1 className='text-center text-orange-600 text-3xl font-semibold mt-10 border border-orange-600 rounded-full py-4 px-8'>About Us</h1>
-            <div className='bg-white m-10 w-96 h-[850px] md:w-[800px] md:h-[500px] rounded-xl flex flex-col md:flex-row items-center justify-center'>
-                <Image src='/benefit.svg' width={280} height={447} alt='' className='md:ml-10 md:mr-20 mb-4'></Image>
-                <div className='grid grid-row md:gap-4'>
-                {benefit.map((poin, index) => (
-                    <div key={index} className='mr-10 ml-4 md:ml-0'>
+            <div className='bg-white border shadow-md shadow-orange-600 m-10 w-96 h-[920px] md:w-[850px] md:h-[500px] rounded-xl flex flex-col md:flex-row items-center justify-center'>
+                <Image src='/about.svg' width={280} height={447} alt='' className='md:hidden md:ml-10 md:mr-20 mb-4'></Image>
+                <div>
+                    <div className='mx-6 md:ml-20'>
                         <div className='flex items-center'>
-                            <Image src='/check.svg' width={30} height={30} alt=''></Image>
-                            <h6 className='text-black font-semibold text-lg ml-4'>{poin.name}</h6>
+                            <h6 className='text-black font-semibold text-xl ml-[70px] md:mb-4'>Riseru Hobby Shop</h6>
                         </div>
-                        <h6 className='text-zinc-600 ml-[46px] text-justify'>{poin.desc}</h6>
+                        <h6 className='text-zinc-600 text-justify mt-3 md:mt-0'>At Riseru Hobby Shop, we are passionate about bringing joy and excitement to the world of hobbies. Whether you're a seasoned collector or a curious beginner, we are here to provide you with a wide range of quality products and exceptional customer service. <br className='mb-3'/>We prioritize your satisfaction. Our user-friendly website offers convenient browsing and ordering, while secure and diverse payment options and reliable packaging to ensure your purchases arrive in excellent condition. Experience seamless and enjoyable shopping from the comfort of your own home.
+                        </h6>
                     </div>
-                ))}
             </div>
+            <Image src='/about.svg' width={280} height={447} alt='' className='hidden md:block md:ml-10 md:mr-20 mb-4'></Image>
             </div>
             {/*  */}
         </section>
--- components/Available.tsx
+++ components/Available.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Available = () => {
-    return <section id="available" className="bg-white w-full h-40">
-        <h1 className='text-black'>Available On</h1>
-    </section>;
+    return (
+        <section id="available" className="bg-zinc-100 w-full flex flex-col items-center justify-center">
+            <h1 className="text-center text-black text-3xl font-semibold mt-10 border rounded-full py-4 px-8">Available On</h1>
+            <div className='grid grid-rows-3 md:grid-cols-3 mt-10 mb-20 gap-5'>
+                <Link href='https://www.instagram.com/riseru_hobbyshop/'>
+                    <Image src='/instagram.png' width={200} height={108.82} alt='Instagram Riseru' className='border-2 rounded-lg transform transition duration-500 hover:scale-105 hover:shadow-lg'/>
+                </Link>
+                <Link href='https://www.tokopedia.com/riseruhobbyshop'>
+                    <Image src='/tokopedia.png' width={200} height={108.82} alt='Instagram Riseru' className='border-2 rounded-lg transform transition duration-500 hover:scale-105 hover:shadow-lg'/>
+                </Link>
+                <Link href='https://www.shopee.co.id/'>
+                    <Image src='/shopee.png' width={200} height={108.82} alt='Instagram Riseru' className='border-2 rounded-lg transform transition duration-500 hover:scale-105 hover:shadow-lg'/>
+                </Link>
+            </div>
+        </section>
+    );
 }
 
 export default Available
