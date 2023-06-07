51fe85b98971862aeb036e9a6e0d2c909a4c22ac
jehianth <jehianbusiness@gmail.com>
2023-06-08 05:59:44 +0700

    fix: about us and availabe on

--- components/Available.tsx
+++ components/Available.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 const Available = () => {
     return (
         <section id="available" className="bg-zinc-100 w-full flex flex-col items-center justify-center">
-            <h1 className="text-center text-black text-3xl font-semibold mt-10 border rounded-full py-4 px-8">Available On</h1>
+            <h1 className="text-center text-black text-3xl font-semibold mt-10 bg-white border shadow-sm shadow-orange-600 rounded-full py-4 px-8">Available On</h1>
             <div className='grid grid-rows-3 md:grid-cols-3 mt-10 mb-20 gap-5'>
                 <Link href='https://www.instagram.com/riseru_hobbyshop/'>
                     <Image src='/instagram.png' width={200} height={108.82} alt='Instagram Riseru' className='border-2 rounded-lg transform transition duration-500 hover:scale-105 hover:shadow-lg'/>
