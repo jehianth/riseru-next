c858ce948186fbbecff4870178900b4b00d39a55
jehianth <jehianbusiness@gmail.com>
2023-06-10 10:03:24 +0700

    fix: section why choose us and about us frame make wider

--- components/Aboutus.tsx
+++ components/Aboutus.tsx
@@ -6,18 +6,18 @@ const Aboutus = () => {
     return (
         <section id='aboutus' className='bg-white w-full flex flex-col items-center justify-center'>
             <h1 className='text-center text-orange-600 text-3xl font-semibold mt-10 border border-orange-600 rounded-full py-4 px-8'>About Us</h1>
-            <div className='bg-white border shadow-md shadow-orange-600 my-10 mx-4 md:m-10 h-[920px] md:w-[850px] md:h-[500px] rounded-xl flex flex-col md:flex-row items-center justify-center'>
+            <div className='bg-white border shadow-md shadow-orange-600 my-10 mx-4 md:m-10 h-[920px] md:w-[1000px] md:h-[500px] rounded-xl flex flex-col md:flex-row items-center justify-center'>
                 <Image src='/about.svg' width={280} height={447} alt='' className='md:hidden md:ml-10 md:mr-20 mb-4'></Image>
                 <div>
-                    <div className='mx-6 md:ml-20'>
+                    <div className='mx-6 md:ml-28'>
                         <div className='flex items-center'>
-                            <h6 className='text-black font-semibold text-xl ml-28 md:ml-[70px] md:mb-4'>Riseru Hobby Shop</h6>
+                            <h6 className='text-black font-semibold text-xl ml-28 md:ml-[100px] md:mb-4'>Riseru Hobby Shop</h6>
                         </div>
                         <h6 className='text-zinc-600 text-justify mt-3 md:mt-0'>At Riseru Hobby Shop, we are passionate about bringing joy and excitement to the world of hobbies. Whether you're a seasoned collector or a curious beginner, we are here to provide you with a wide range of quality products and exceptional customer service. <br className='mb-3'/>We prioritize your satisfaction. Our user-friendly website offers convenient browsing and ordering, while secure and diverse payment options and reliable packaging to ensure your purchases arrive in excellent condition. Experience seamless and enjoyable shopping from the comfort of your own home.
                         </h6>
                     </div>
             </div>
-            <Image src='/about.svg' width={280} height={447} alt='' className='hidden md:block md:ml-10 md:mr-20 mb-4'></Image>
+            <Image src='/about.svg' width={280} height={447} alt='' className='hidden md:block md:ml-20 md:mr-28 mb-4'></Image>
             </div>
             {/*  */}
         </section>
--- components/Whychooseus.tsx
+++ components/Whychooseus.tsx
@@ -23,8 +23,8 @@ const Whychooseus = () => {
     return (
         <section id='whychooseus' className='bg-orange-600 w-full flex flex-col items-center justify-center'>
             <h1 className='text-center text-3xl font-semibold mt-10 border rounded-full py-4 px-8 text-white'>Why Choose Us?</h1>
-            <div className='bg-white my-10 mx-4 md:m-10 h-[850px] md:w-[800px] md:h-[500px] rounded-xl flex flex-col md:flex-row items-center justify-center'>
-                <Image src='/benefit.svg' width={280} height={447} alt='' className='md:ml-10 md:mr-20 mb-4'></Image>
+            <div className='bg-white my-10 mx-4 md:m-10 h-[850px] md:w-[1000px] md:h-[500px] rounded-xl flex flex-col md:flex-row items-center justify-center'>
+                <Image src='/benefit.svg' width={280} height={447} alt='' className='md:ml-28 md:mr-20 mb-4'></Image>
                 <div className='grid grid-row md:gap-4'>
                 {benefit.map((poin, index) => (
                     <div key={index} className='mr-10 ml-4 md:ml-0'>
@@ -32,7 +32,7 @@ const Whychooseus = () => {
                             <Image src='/check.svg' width={30} height={30} alt='' className='ml-8 md:ml-0'></Image>
                             <h6 className='text-black font-semibold text-lg ml-4'>{poin.name}</h6>
                         </div>
-                        <h6 className='text-zinc-600 ml-20 md:ml-[46px] text-justify'>{poin.desc}</h6>
+                        <h6 className='text-zinc-600 ml-20 md:ml-[46px] md:w-9/12 md:text-justify'>{poin.desc}</h6>
                     </div>
                 ))}
             </div>
